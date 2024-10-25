1bb3e77975949770fb41a9b27be05c8a8b29334f
visstech <122130687+visstech@users.noreply.github.com>
2024-10-25 08:40:32 +0800

    Update StreamlitTicketAnalysis.py

--- StreamlitTicketAnalysis.py
+++ StreamlitTicketAnalysis.py
@@ -5,7 +5,7 @@ import streamlit as st
 import seaborn as sns 
 import matplotlib.pyplot as plt 
 import numpy as np 
-#data = pd.read_excel('C:\\Users\\visse\\OneDrive - iorta TechNXT\\2024\\TIGB\OCT-2024\\11-OCT-2024\\TIGB_TICKET_STATUS_AGEWISE_AS_ON_10_OCT_24.xlsx')
+
 data = pd.read_csv('TIGB_TICKET_STATUS_AGEWISE.csv');
 print(data)
 color = st.sidebar.color_picker('Choose a background color', '#00f900')
